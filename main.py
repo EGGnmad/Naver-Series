a3a5cc0f72b0625b8cef8efffd4f733661a3afa9
EGGnmad <viewnono1219@gmail.com>
2021-12-22 20:52:45 +0900

    edit main.py

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import NaverSeries
 
 if __name__ == '__main__':
     #search
-    print(NaverSeries.getInfo(5133669))
+    book = NaverSeries.search(input('search: '), focus='comic')
+    book_id = book['contents'][0]['id']
+
+    print(NaverSeries.getInfo(book_id))
