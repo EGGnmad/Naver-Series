9476b8bbed1a4d30dba92ee26545ccf89d24e51c
EGGnmad <viewnono1219@gmail.com>
2021-12-22 15:53:45 +0900

    add setup

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='Naver Series module',
     version='1.0.0 Alpha',
+    description='Naver Series module',
 
     author='EGGnmad',
     author_email='viewnono1219@gmail.com',
+    url='https://github.com/EGGnmad/NaverSeries',
 
     packages=find_packages(),
 
