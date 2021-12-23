b8d15b97a11fc8a3058b2bff4e92e6209d42be33
EGGnmad <viewnono1219@gmail.com>
2021-12-23 17:47:35 +0900

    edit setup.py [-requires, +install_requires]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Naver_Series',
-    version='1.1.0 Beta',
+    version='1.1.3 Beta',
     description='Naver Series module',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -14,5 +14,5 @@ setup(
     packages=find_packages(),
     keywords=['Naver', 'Naver Series', 'Book'],
 
-    requires=['requests', 'beautifulsoup4']
+    install_requires=['requests==2.26.0', 'beautifulsoup4==4.10.0']
 )
